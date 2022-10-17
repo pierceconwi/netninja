122a8ac17713fd29e5880d68b71fdcf1e2909e5f
Pierce Conwi <pierceconwi@gmail.com>
2022-10-17 15:21:00 -0700

    tutorial no. 18 complete

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Microblog with React.js based on TheNetNinja tutorial, found here:
 
 https://www.youtube.com/playlist?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d
+
+Enable json watching via node command:
+
+npx json-server --watch data/db.json --port 8000
+
+This will watch file and wrap it with API endpoints
--- src/Home.js
+++ src/Home.js
@@ -7,12 +7,9 @@ const Home = () => {
     const [name, setName] = useState("visitor");
 
     // React state set-up for blog data:
-    const [blogs, setBlogs] = useState([
-        {title: 'My New Website', body: 'Our new blog is finally live! Please look forward to more updates!', author: 'mario', id: 1},
-        {title: 'Welcome Party', body: 'To celebrate the launch of our new blog, we will be chugging caviar in the break room this Friday at 6 PM.', author: 'yoshi', id: 2},
-        {title: 'Web Dev Top Tips', body: 'Since we lost so many developers to caviar poisoning last Friday, I would like to step in with some quick dev tips to help you recent hires settle into your new roles.', author: 'mario', id: 3}
-    ]);
-
+    const [blogs, setBlogs] = useState(null);
+    // React state set-up for data
+    const [isLoading, setIsLoading] = useState(true);
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs);
@@ -20,9 +17,19 @@ const Home = () => {
 
     // runs every render of this component (initial render, plus when React State changes - every subsequent render)
     useEffect(() => {
-        console.log("use effect ran");
-        console.log(name);
-    }, [name]);
+        // grab json data via endpoint. fetch() returns a promise, so okay to use .then
+        fetch('http://localhost:8000/blogs')
+            // take the promise and converts to json
+            .then(res => {
+                return res.json(); 
+            })
+            // take result of previous return and use setBlogs() to render the now-ready json data
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+                setIsLoading(false);
+            })
+    }, []);
 
     return (
         <div className="home">
@@ -32,9 +39,8 @@ const Home = () => {
             <br />
             <button onClick={() => setName('button clicker')}>Click me</button>
             <hr />
-           <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-           {/* Blogs filtered by author via id:
-           <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"/> */}
+            {isLoading && <div>Loading...</div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
         </div>
      );
 }
