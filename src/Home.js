2299ee1c18583f001a898fa06846cefb50b59ede
Pierce Conwi <pierceconwi@gmail.com>
2022-10-17 11:43:44 -0700

    tutorial no. 9 complete

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,30 @@
+import { useState } from 'react';
+
 const Home = () => {
-    let name = 'mario';
+
+    const [name, setName] = useState("visitor");
+    const [age, setAge] = useState(25);
+
 
     const handleClick = (e) => {
-        console.log('Hello, punks', e);
-    };
-        /* Below: (r) is the received value of (e) from the onClick anonymous function */
-    const handleClickAgain = (name, r) => {
-        console.log("Hello " + name, r.target);
+        setName("button master");
+        setAge(34);
     };
 
-    return ( 
+        /* Below: (r) is the received value of (e) from the onClick anonymous function
+    const handleClickAgain = (name, r) => {
+        console.log("Hello " + name, r.target);
+    }; */
+    return (
         <div className="home">
             <h2>Homepage</h2>
             <br />
             <p>Hello, { name }!</p>
+            <p>You are { age } years old.</p>
             <br />
             <button onClick={handleClick}>Click me</button>
-            {/* Below: (e) is the click event, and can be passed like so back into the handeClickAgain() func for use within */}
-            <button onClick={(e) => {handleClickAgain('pierce', e)}}>Click me again</button>
+            {/* Below: (e) is the click event, and can be passed like so back into the handeClickAgain() func for use within 
+            <button onClick={(e) => {handleClickAgain('pierce', e)}}>Click me again</button> */}
         </div>
      );
 }
